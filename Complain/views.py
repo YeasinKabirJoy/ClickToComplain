0d9abc6cfae99721212bf20b6a31c62ae16541f1
Yeasin Kabir Joy <18101020@uap-bd.edu>
2021-02-28 19:53:17 +0600

    update

--- Complain/views.py
+++ Complain/views.py
@@ -12,29 +12,33 @@ from django.shortcuts import render, get_object_or_404,redirect
 # Create your views here.
 @login_required
 def complainForm(request):
-    user_profile = Profile.objects.get(user=request.user)
-    if user_profile.status:
-        complain_form = ComplainForm()
-        msg = ''
-
-        if request.method == 'POST':
-            complain_form = ComplainForm(request.POST, request.FILES)
-            msg = 'Invalid input'
-
-            if complain_form.is_valid():
-                complain = complain_form.save(commit=False)
-                complain.user = request.user
-                complain.save()
-                for t in request.POST.getlist('tag'):
-                    complain.tag.add(t)
-                complain_form = ComplainForm()
-                msg = 'Your complain has been recorded!'
-            else:
-                complain_form = ComplainForm()
-                msg = 'Invalid input! Please try again!'
+    if  request.user.is_superuser:
+        complain_form=''
+        msg='Go to Admin Panel'
     else:
-        complain_form = ''
-        msg = 'Sorry !! You are not verified yet!'
+        user_profile = Profile.objects.get(user=request.user)
+        if user_profile.status:
+            complain_form = ComplainForm()
+            msg = ''
+
+            if request.method == 'POST':
+                complain_form = ComplainForm(request.POST, request.FILES)
+                msg = 'Invalid input'
+
+                if complain_form.is_valid():
+                    complain = complain_form.save(commit=False)
+                    complain.user = request.user
+                    complain.save()
+                    for t in request.POST.getlist('tag'):
+                        complain.tag.add(t)
+                    complain_form = ComplainForm()
+                    msg = 'Your complain has been recorded!'
+                else:
+                    complain_form = ComplainForm()
+                    msg = 'Invalid input! Please try again!'
+        else:
+            complain_form = ''
+            msg = 'Sorry !! You are not verified yet!'
 
     context = {
         'complain_form': complain_form,
@@ -78,11 +82,16 @@ def commentForm(request):
 
 @login_required
 def showComplain(request):
-    user_profile = Profile.objects.get(user=request.user)
-    if user_profile.status:
-        complain = reversed(Complain.objects.filter(private=False, status= 'Pending'))
+    if not request.user.is_superuser:
+        user_profile = Profile.objects.get(user=request.user)
+        if user_profile.status:
+            complain =Complain.objects.filter(private=False, status= 'Pending')
+            complain=reversed(complain)
+        else:
+           complain = 'Sorry !! You are not verified yet!'
     else:
-       complain = 'Sorry !! You are not verified yet!'
+        complain = Complain.objects.filter(private=False, status='Pending')
+        complain=reversed(complain)
 
     context = {
         'complain': complain,
@@ -93,24 +102,66 @@ def showComplain(request):
 
 @login_required
 def showSolvedComplain(request):
-    user_profile = Profile.objects.get(user=request.user)
-    if user_profile.status:
-        solvedComplain = reversed(Complain.objects.filter(private=False, status= 'Solved'))
+    if not request.user.is_superuser:
+        user_profile = Profile.objects.get(user=request.user)
+        if user_profile.status:
+            solvedComplain = Complain.objects.filter(private=False, status= 'Solved')
+            solvedComplain=reversed(solvedComplain)
+        else:
+            solvedComplain = 'Sorry !! You are not verified yet!'
     else:
-        solvedComplain = 'Sorry !! You are not verified yet!'
-
+        solvedComplain = Complain.objects.filter(private=False, status='Solved')
+        solvedComplain=reversed(solvedComplain)
     context = {
         'complain': solvedComplain,
     }
     return render(request, 'Complain/solvedComplains.html', context)
 
 @login_required
+
 def complainDetails(request, complain_id):
+    adminMsg = ''
     complain = get_object_or_404(Complain, id=complain_id)
     vote = ''
+    if not request.user.is_superuser:
+        user_profile = Profile.objects.get(user=request.user)
+        if user_profile.status:
+            comment_form = CommentForm()
+            msg = ''
+            if request.method == 'POST':
+                comment_form = CommentForm(request.POST)
+                msg = 'Invalid input'
 
-    user_profile = Profile.objects.get(user=request.user)
-    if user_profile.status:
+                if comment_form.is_valid():
+                    comment = comment_form.save(commit=False)
+                    if comment != '':
+                        comment.user = user_profile
+                        comment.save()
+                        msg = 'Insertion done!'
+                        comment_form = CommentForm()
+                else:
+                    comment_form = CommentForm()
+                    msg = 'Invalid input! Please try again!'
+
+
+                vote = request.POST.get("vote")
+
+                if complain.votes.exists(request.user.id):
+                    pass
+                else:
+                    if vote == "upvote":
+                        complain.votes.up(request.user.id)
+
+                    elif vote == "downvote":
+                        complain.votes.down(request.user.id)
+                    else:
+                        pass
+
+        else:
+            comment_form = ''
+            msg = 'Sorry !! You are not verified yet!'
+
+    else:
         comment_form = CommentForm()
         msg = ''
         if request.method == 'POST':
@@ -120,39 +171,21 @@ def complainDetails(request, complain_id):
             if comment_form.is_valid():
                 comment = comment_form.save(commit=False)
                 if comment != '':
-                    comment.user = user_profile
+                    comment.user = request.user
                     comment.save()
                     msg = 'Insertion done!'
                     comment_form = CommentForm()
             else:
                 comment_form = CommentForm()
                 msg = 'Invalid input! Please try again!'
-
-
-            vote = request.POST.get("vote")
-
-            if complain.votes.exists(request.user.id):
-                pass
-            else:
-                if vote == "upvote":
-                    complain.votes.up(request.user.id)
-
-                elif vote == "downvote":
-                    complain.votes.down(request.user.id)
-                else:
-                    pass
-
-    else:
-        comment_form = ''
-        msg = 'Sorry !! You are not verified yet!'
-
-
+        adminMsg='admin'
 
     context = {
         'complain': complain,
         'vote': vote,
         'comment_form': comment_form,
-        'msg': msg
+        'msg': msg,
+        'adminMsg':adminMsg
     }
 
     return render(request, 'Complain/complainDetails.html', context)
---
+++ UserManagement/context.py
@@ -0,0 +1,22 @@
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+from .models import Profile
+
+def verifiedUser(request):
+    userStatus=''
+    try:
+        user = get_object_or_404(Profile,user=request.user)
+        if user.status:
+            userStatus = 'Verified'
+        else:
+            userStatus = 'notVerified'
+    except Exception:
+        userStatus = 'Admin'
+
+
+
+    context = {
+        'userStatus':userStatus
+    }
+    return context
